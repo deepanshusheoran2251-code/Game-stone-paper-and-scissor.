4446bec9512b022d0063aa3b32460db79fc683b3
Deepanshu <deepanshusheoran2251@gmail.com>
2026-04-21 21:22:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+def Game():
+    # user=input("Enter the name of player:")
+    while True:
+       
+        print("\n1.STONE")
+        print("\n2.PAPER")
+        print("\n3.SCISSOR")
+        
+        player=input(f"\nEnter the choice : ")
+        robot=random.randint(1,3)
+        
+        if player.isdigit():
+            player=int(player)
+            if player==0:
+                print("\nGame is over👋")
+                break
+            if player not in[1,2,3]:
+                 print("\n❌Invalid Choice❌")
+
+            print(f"\nRobot choice is: {robot}")
+
+            if player==1 and robot==2 or player==2 and robot==3 or player==3 and robot==1:
+                print("\nResult :ROBOT🤖 is the Winner")
+            elif player==1 and robot==3 or player==2 and robot==1 or player==3 and robot==2:
+                print("\nResult :player🚹 is the Winner")
+            else: 
+                print("\nResult :Match is DRAWN🟰")
+            
+               
+        else:
+            print("\n❌Enter a valid Choice❌")
+
+print("__WELCOME IN THE GAME🎮-STONE,PAPER,SCISSOR__")
+Game()
